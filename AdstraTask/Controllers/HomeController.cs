bf5adb2ff1abe333a29bc3aa917c9461d4455e4d
unknown <shifa.rababaah@gmail.com>
2023-05-15 23:40:36 +0300

    update identity  user

--- AdstraTask/Controllers/HomeController.cs
+++ AdstraTask/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using AdstraTask.Data;
-using AdstraTask.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+﻿using AdstraTask.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -10,37 +7,14 @@ namespace AdstraTask.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-         UserManager<IdentityUser> _userManager;
-        RoleManager<IdentityRole> _roleManager;
-        ApplicationDbContext _identity;
 
-        public HomeController(ILogger<HomeController> logger , UserManager<IdentityUser> userManager,
-            RoleManager<IdentityRole> roleManager, ApplicationDbContext identity)
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _userManager = userManager;
-            _roleManager = roleManager;
-            _roleManager = roleManager;
-            _identity = identity;
-
         }
 
         public IActionResult Index()
         {
-            //if (_identity.Roles.FirstOrDefault(x => x.Name == "Admin") == null)
-            //{
-            //    _identity.Roles.Add(new IdentityRole { Name = "Admin", NormalizedName = "Admin" });
-            //    _identity.SaveChanges();
-            //}
-
-            //if (_identity.Roles.FirstOrDefault(x => x.Name == "User") == null)
-            //{
-            //    _identity.Roles.Add(new IdentityRole { Name = "User", NormalizedName = "User" });
-            //    _identity.SaveChanges();
-            //}
-
-
-
             return View();
         }
 
@@ -48,13 +22,6 @@ namespace AdstraTask.Controllers
         {
             return View();
         }
-        [Authorize(Roles = "Admin")]
-        public IActionResult ShowUsersInfo()
-        {
-            var users = _userManager.Users.ToList();
-            return View(users);
-        }
-
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
